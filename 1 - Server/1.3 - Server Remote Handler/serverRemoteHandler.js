3c207b1df349dae90e615e4e67db34fca222bc96
cvonayres <carl.ayres@live.co.uk>
2024-01-29 21:54:09 +0000

    Day 2b - added gitignore, NPM. Installed express, axios and body-Parser. Added serverDataHandler port number to serverRemoteHandler.

--- 1 - Server/1.3 - Server Remote Handler/serverRemoteHandler.js
+++ 1 - Server/1.3 - Server Remote Handler/serverRemoteHandler.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 66000;
+const API_URL = "http://localhost:56000";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
