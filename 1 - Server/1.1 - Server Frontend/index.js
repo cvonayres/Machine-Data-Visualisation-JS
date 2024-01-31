e5b85a4b3e5071d8b3ad86b273c2087bf0af2eb3
cvonayres <carl.ayres@live.co.uk>
2024-01-31 22:47:12 +0000

    Got index working, links to css and js working to provide sliding animation to webpage.

--- 1 - Server/1.1 - Server Frontend/index.js
+++ 1 - Server/1.1 - Server Frontend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 32000;
 const API_URL = "http://localhost:56000";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathIndex = __dirname + "/views/index.ejs"
 
 app.use(express.static("public"));
 
@@ -13,16 +17,17 @@ app.use(bodyParser.json());
 
 // Route to render the main page
 app.get("/", async (req, res) => {
-    try {
-      const response = await axios.get(`${API_URL}/index`);
-      console.log(response);
-      res.render("index.ejs", { posts: response.data });
-    } catch (error) {
-      res.status(500).json({ message: "Error fetching /index" });
-    }
-  });
+  res.render(pathIndex);
+  // try {
+  //   const response = await axios.get(`${API_URL}/index`);
+  //   console.log(response);
+  //   res.render("index.ejs", { posts: response.data });
+  // } catch (error) {
+  //   res.status(500).json({ message: "Error fetching /index" });
+  // }
+});
   
-  app.listen(port, () => {
-    console.log(`Backend server for the front-end is running on http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Backend server for the front-end is running on http://localhost:${port}`);
+});
   
